c9908bb6c8207e0ff444be2c3b50ff6949e338cb
Jakub Sierant <90271504+klvska@users.noreply.github.com>
2024-02-09 17:41:15 +0100

    Update anabolicfactory.sql

--- db/anabolicfactory.sql
+++ db/anabolicfactory.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 13 Mar 2023, 22:42
--- Wersja serwera: 10.4.24-MariaDB
--- Wersja PHP: 8.1.6
+-- Generation Time: Feb 09, 2024 at 05:40 PM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Baza danych: `anabolicfactory`
+-- Database: `anabolicfactory`
 --
 
 -- --------------------------------------------------------
@@ -34,35 +34,36 @@ CREATE TABLE `items` (
   `item_price` varchar(255) NOT NULL,
   `item_old_price` varchar(255) NOT NULL,
   `item_alt` varchar(255) NOT NULL,
-  `item_category` varchar(55) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `item_category` varchar(55) NOT NULL,
+  `item_quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `items`
+-- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`item_id`, `item_name`, `item_url`, `item_price`, `item_old_price`, `item_alt`, `item_category`) VALUES
-(1, 'KFD Premium WPC 82 o smaku karmelowego - mleczka 700g', './img/whey/wpc-82.png', '79.99zł', '89.99zł\r\n', 'Białko KFD Premium WPC 82 różne smaki', 'promos'),
-(2, 'KFD Monohydrat Kreatyny o smaku Marihuaen 500g', './img/creatine/420.jpg', '69.99zł', '79.99zł', 'Kreatyna KFD Monohydrat o smaku Maruhuaen', 'promos'),
-(3, 'WK DZIK Bomba przedtreningówka o smaku coli 150g', './img/others/bomba.jpg', '49.99zł', '59.99zł', 'WK DZIK w pani domu jest bomba', 'promos'),
-(4, 'Heisenberg przedtreningówka z DMAA 200g', './img/others/heisenberg.jpg', '129.99zł', '149.99zł\r\n', 'Włodzimierz Biały bombowy preworkout z dmaa', 'promos'),
-(5, 'KFD Clear Whey 90 sex on the beach 420g', './img/whey/clear-whey.jpg', '79.99zł', '', 'Białko KFD Clear Whey 90 sex-on the beach', 'newests'),
-(6, 'RL9 Krem Orzechowo - Czekoladowy 200g', './img/others/rl9.jpg', '69.99zł', '', 'Krem orzechowo - czekoladowy RL9', 'newests'),
-(7, 'KFD Koenzym Q10 - 100 Kaps.', './img/others/coenzyme.jpg', '49.99zł', '', 'KFD Koenzym', 'newests'),
-(8, 'KFD Dżemik z czerwonych owoców 1000g', './img/others/dzemik.jpg', '129.99zł', '', 'KFD Dżemik z czerwonych owoców', 'newests'),
-(9, 'KFD Premium WPC 80 różne smaki 750g', './img/whey/wpc-80.jpg', '63.99zł', '', 'Białko KFD Premium WPC 80 różne smaki', 'whey'),
-(10, 'KFD PREMIUM WPI 90 różne smaki 700g', './img/whey/wpi-90.jpg', '89.99zł', '', 'Białko KFD Premium WPI 90 różne smaki', 'whey'),
-(11, 'Olimp Protein Complex 100% różne smaki 700g', './img/whey/olimp-whey.jpg', '95.99zł', '', 'Białko Olimp Protein Complex 100% różne smaki', 'whey'),
-(12, 'WK DZIK Dobre Whey PREMIUM różne smaki 900g', './img/whey/dobre-whey.jpg', '99.99zł', '', 'Białko WK DZIK różne smaki', 'whey'),
-(13, 'KFD Premium Creatine różne smaki 500g', './img/creatine/creatine500.jpg', '79.99zł', '', 'KFD Premium Creatine różne smaki 500g', 'creatine'),
-(14, 'KFD Premium Creatine różne smaki 250g', './img/creatine/creatine250.jpg', '59.99zł', '', 'KFD Premium Creatine różne smaki 250g', 'creatine'),
-(15, 'Olimp kre alkalyn 2500 mega caps 120 kaps', './img/creatine/kre-alakyn.jpg', '159.99zł', '', 'Kreatyna kre-alakyn Monohydrat', 'creatine'),
-(16, 'WK DZIK Monohydrat kreatyny 225g', './img/creatine/wk-kreatyna.jpg', '59.99zł', '', 'WK DZIK Monohydrat kreatyny', 'creatine'),
-(17, 'KFD Suplement diety Magnesium+ 160 Kaps.', './img/others/magnez.jpg', '18.99zł', '', 'KFD Magnesium', 'others'),
-(18, 'KFD Krem orzechowy z arachidów 100% 1000g', './img/others/mrmasel.jpg', '23.99zł', '', 'KFD MrMasel', 'others'),
-(19, 'KFD Przyprawa do mięs 180g', './img/others/przyprawa.jpg', '8.99zł', '', 'KFD przyprawa do mies', 'others'),
-(20, 'KFD erytrytol zamiennik cukru 1000g', './img/others/erytrytol.jpg', '24.99zł', '', 'KFD Erytrytol', 'others'),
-(21, 'Sos KFD', './img/others/sos.jpg', '11.90zł', '', 'Sos KFD o smaku truskawkowym', '');
+INSERT INTO `items` (`item_id`, `item_name`, `item_url`, `item_price`, `item_old_price`, `item_alt`, `item_category`, `item_quantity`) VALUES
+(1, 'KFD Premium WPC 82 o smaku karmelowego - mleczka 700g', './img/whey/wpc-82.png', '79.99zł', '89.99zł\r\n', 'Białko KFD Premium WPC 82 różne smaki', 'promos', 1000),
+(2, 'KFD Monohydrat Kreatyny o smaku Marihuaen 500g', './img/creatine/420.jpg', '69.99zł', '79.99zł', 'Kreatyna KFD Monohydrat o smaku Maruhuaen', 'promos', 999),
+(3, 'WK DZIK Bomba przedtreningówka o smaku coli 150g', './img/others/bomba.jpg', '49.99zł', '59.99zł', 'WK DZIK w pani domu jest bomba', 'promos', 1000),
+(4, 'Heisenberg przedtreningówka z DMAA 200g', './img/others/heisenberg.jpg', '129.99zł', '149.99zł\r\n', 'Włodzimierz Biały bombowy preworkout z dmaa', 'promos', 994),
+(5, 'KFD Clear Whey 90 sex on the beach 420g', './img/whey/clear-whey.jpg', '79.99zł', '', 'Białko KFD Clear Whey 90 sex-on the beach', 'newests', 1000),
+(6, 'RL9 Krem Orzechowo - Czekoladowy 200g', './img/others/rl9.jpg', '69.99zł', '', 'Krem orzechowo - czekoladowy RL9', 'newests', 1000),
+(7, 'KFD Koenzym Q10 - 100 Kaps.', './img/others/coenzyme.jpg', '49.99zł', '', 'KFD Koenzym', 'newests', 1000),
+(8, 'KFD Dżemik z czerwonych owoców 1000g', './img/others/dzemik.jpg', '129.99zł', '', 'KFD Dżemik z czerwonych owoców', 'newests', 1000),
+(9, 'KFD Premium WPC 80 różne smaki 750g', './img/whey/wpc-80.jpg', '63.99zł', '', 'Białko KFD Premium WPC 80 różne smaki', 'whey', 1005),
+(10, 'KFD PREMIUM WPI 90 różne smaki 700g', './img/whey/wpi-90.jpg', '89.99zł', '', 'Białko KFD Premium WPI 90 różne smaki', 'whey', 1000),
+(11, 'Olimp Protein Complex 100% różne smaki 700g', './img/whey/olimp-whey.jpg', '95.99zł', '', 'Białko Olimp Protein Complex 100% różne smaki', 'whey', 1000),
+(12, 'WK DZIK Dobre Whey PREMIUM różne smaki 900g', './img/whey/dobre-whey.jpg', '99.99zł', '', 'Białko WK DZIK różne smaki', 'whey', 1000),
+(13, 'KFD Premium Creatine różne smaki 500g', './img/creatine/creatine500.jpg', '79.99zł', '', 'KFD Premium Creatine różne smaki 500g', 'creatine', 1000),
+(14, 'KFD Premium Creatine różne smaki 250g', './img/creatine/creatine250.jpg', '59.99zł', '', 'KFD Premium Creatine różne smaki 250g', 'creatine', 1000),
+(15, 'Olimp kre alkalyn 2500 mega caps 120 kaps', './img/creatine/kre-alakyn.jpg', '159.99zł', '', 'Kreatyna kre-alakyn Monohydrat', 'creatine', 1000),
+(16, 'WK DZIK Monohydrat kreatyny 225g', './img/creatine/wk-kreatyna.jpg', '59.99zł', '', 'WK DZIK Monohydrat kreatyny', 'creatine', 1000),
+(17, 'KFD Suplement diety Magnesium+ 160 Kaps.', './img/others/magnez.jpg', '18.99zł', '', 'KFD Magnesium', 'others', 1000),
+(18, 'KFD Krem orzechowy z arachidów 100% 1000g', './img/others/mrmasel.jpg', '23.99zł', '', 'KFD MrMasel', 'others', 1000),
+(19, 'KFD Przyprawa do mięs 180g', './img/others/przyprawa.jpg', '8.99zł', '', 'KFD przyprawa do mies', 'others', 1000),
+(20, 'KFD erytrytol zamiennik cukru 1000g', './img/others/erytrytol.jpg', '24.99zł', '', 'KFD Erytrytol', 'others', 1000),
+(21, 'Sos KFD', './img/others/sos.jpg', '11.90zł', '', 'Sos KFD o smaku truskawkowym', '', 1000);
 
 -- --------------------------------------------------------
 
@@ -76,15 +77,16 @@ CREATE TABLE `users` (
   `user_email` varchar(255) NOT NULL,
   `user_passwordhash` varchar(255) NOT NULL,
   `is_admin` tinyint(1) DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Zrzut danych tabeli `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`user_id`, `user_nickname`, `user_email`, `user_passwordhash`, `is_admin`) VALUES
 (1, 'admin', 'admin@gmail.com', '$2y$10$AzsxLXnM3uVYnax2544eyO1MHvRaX7qGWv4xjFLX8ERlml7bjjW7.', 1),
-(2, 'klvsk4', 'test@test.com', '$2y$10$KiOv6eFVbCws9PMWBZPRdOajFxpuQ.VWp4cv4YBPlCX5fvv9yyrBK', 0);
+(2, 'klvsk4', 'test@test.com', '$2y$10$KiOv6eFVbCws9PMWBZPRdOajFxpuQ.VWp4cv4YBPlCX5fvv9yyrBK', 0),
+(3, 'tester', 'tester@test.com', '$2y$10$M3wdivyOsQwKVcH7jybVEOqhOszElQ2oe49VSxefcHvF5wB7glIci', 0);
 
 --
 -- Indeksy dla zrzutów tabel
@@ -104,20 +106,20 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `user_email` (`user_email`);
 
 --
--- AUTO_INCREMENT dla zrzuconych tabel
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT dla tabeli `items`
+-- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
   MODIFY `item_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
--- AUTO_INCREMENT dla tabeli `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
